1c79625bab283747639f66f77fdb272b5ad5bb7c
99apollo <99apollo@naver.com>
2023-03-08 22:42:58 +0900

    퀴즈 버튼 + 선택지 추가

--- app/src/main/java/com/example/restapitest/MainActivity.kt
+++ app/src/main/java/com/example/restapitest/MainActivity.kt
@@ -11,14 +11,17 @@ import org.json.JSONArray
 
 class MainActivity : AppCompatActivity() {
     var tv1: TextView? = null
+    var tv2: TextView? = null
     var locate:Int=0
     var total:Int=0
+
     var templist=ArrayList<Quiz>()
     @SuppressLint("ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         tv1=findViewById(R.id.text_test1)
+        tv2=findViewById(R.id.quiz_body)
         val nb = findViewById<Button>(R.id.next_button)
         // 1. JSON 파일 열어서 String으로 취득
         val jsonString = assets.open("quiz.json").reader().readText()
@@ -37,15 +40,25 @@ class MainActivity : AppCompatActivity() {
             val test=jsonObject.getString("answer").toInt()
             total++
             templist.add(Quiz(id,body,test))
-
+            Log.e("test",templist[index].body)
 
 
         }
-        tv1?.setText(templist[0].title)
+
+        tv1?.setText(templist[locate].title)
+        tv2?.setText(templist[locate].body)
         nb.setOnClickListener {
             locate++
             if (locate < 3){
                 tv1?.setText(templist[locate].title)
+                tv2?.setText(templist[locate].body)
+                Log.e("body",templist[locate].body)
+                if(locate+1==total){
+                    nb.setText("점수 확인")
+                }
+            }
+            if (locate==total){
+                tv1?.setText("0")
             }
         }
 
